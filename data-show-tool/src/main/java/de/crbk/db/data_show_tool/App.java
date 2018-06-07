e67f430d0527e18bbd588c5eec2752a21a8808a5
christophwin <christophwinter97@gmail.com>
2018-06-07 15:35:41 +0200

    initial import

---
+++ data-show-tool/src/main/java/de/crbk/db/data_show_tool/App.java
@@ -0,0 +1,13 @@
+package de.crbk.db.data_show_tool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
