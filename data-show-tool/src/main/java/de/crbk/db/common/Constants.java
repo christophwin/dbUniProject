ab366ce536d80899c56e36e9463e1240592795de
christophwin <christophwinter97@gmail.com>
2018-06-08 14:04:01 +0200

    added properties and log4j file

--- data-show-tool/src/main/java/de/crbk/db/common/Constants.java
+++ data-show-tool/src/main/java/de/crbk/db/common/Constants.java
@@ -3,4 +3,8 @@ package de.crbk.db.common;
 public class Constants
 {
     public static final String MAINFRAME_FXML = "/mainFrame_fxml.fxml";
+
+    public static final String DATABASE_URL_PROP = "databaseUrl";
+
+    public static final String DATABASE_PORT_PROP = "databasePort";
 }
