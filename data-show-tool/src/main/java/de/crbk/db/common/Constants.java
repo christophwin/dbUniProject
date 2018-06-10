1def25c158eb367a2285119def36f36e44e9e29a
christophwin <christophwinter97@gmail.com>
2018-06-10 16:36:42 +0200

    added creation and closing of database connection

--- data-show-tool/src/main/java/de/crbk/db/common/Constants.java
+++ data-show-tool/src/main/java/de/crbk/db/common/Constants.java
@@ -5,8 +5,12 @@ public class Constants
     public static final String MAINFRAME_FXML = "/mainFrame_fxml.fxml";
     
     public static final String LOG4J = "/log4j.xml";
+    
+    public static final String PROPERTIES_FILE = "/conf.properties";
 
     public static final String DATABASE_URL_PROP = "databaseUrl";
 
-    public static final String DATABASE_PORT_PROP = "databasePort";
+    public static final String USER_PROP = "username";
+    
+    public static final String PASSWORD_PROP = "password";
 }
--- data-show-tool/src/main/java/de/crbk/db/controller/UniversityData.java
+++ data-show-tool/src/main/java/de/crbk/db/controller/UniversityData.java
@@ -1,9 +1,19 @@
 package de.crbk.db.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 
 import de.crbk.db.common.Constants;
+import de.crbk.db.controller.exceptions.DataToolException;
 import de.crbk.db.ui.UserMainInterface;
 import javafx.application.Application;
 
@@ -17,6 +27,23 @@ public class UniversityData
 
     private static UniversityData instance = null;
 
+    private Connection databaseConnection = null;
+
+    /**
+     * start method
+     * 
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        UniversityData.getInstance().execute();
+    }
+
+    /**
+     * method to get controller instance
+     * 
+     * @return
+     */
     public static UniversityData getInstance()
     {
         if (instance == null)
@@ -26,26 +53,76 @@ public class UniversityData
         return instance;
     }
 
-    public static void main(String[] args)
-    {
-        UniversityData.getInstance().execute();
-    }
-
+    /**
+     * main execution method to start program
+     */
     public void execute()
     {
         try
         {
             DOMConfigurator.configure(this.getClass().getResource(Constants.LOG4J));
-            Application.launch(UserMainInterface.class);
+            createDatabaseConnection();
+            Application.launch(UserMainInterface.class); // launches the main frame
         }
         catch (Exception e)
         {
-            LOG.error(e);
+            LOG.error(e.getMessage(),e);
         }
         finally
         {
-            //close db connection
+            closeDatabaseConnection();
         }
 
+    }
+
+    /**
+     * create the initial database connection <br>
+     * set 'auto-commit' to 'false'
+     * 
+     * @throws DataToolException if a error occur while connection
+     */
+    private void createDatabaseConnection()
+        throws DataToolException
+    {
+        LOG.info("Start to connect to database. Read properties.");
+        Properties props = new Properties();
+        try
+        {
+            props.load(new FileInputStream(new File(this.getClass().getResource(Constants.PROPERTIES_FILE).toURI())));
+
+            String connectionUrl = props.getProperty(Constants.DATABASE_URL_PROP);
+            LOG.info("URL: " + connectionUrl);
+
+            databaseConnection = DriverManager.getConnection(connectionUrl, props.getProperty(Constants.USER_PROP),
+                                                             props.getProperty(Constants.PASSWORD_PROP));
+
+            databaseConnection.setAutoCommit(false);
+            LOG.info("Database connection is valid: " + databaseConnection.isValid(1000));
+        }
+        catch (IOException | URISyntaxException | SQLException e)
+        {
+            throw new DataToolException("An error occour while creating database connection", e);
+        }
+    }
+
+    /**
+     * close the database connection if one exist <br>
+     * no commit will be executed before
+     */
+    private void closeDatabaseConnection()
+    {
+        LOG.info("Database connection will be closed.");
+
+        if (databaseConnection != null)
+        {
+            try
+            {
+                databaseConnection.close();
+            }
+            catch (SQLException e)
+            {
+                LOG.error("Error while closing database connection.", e);
+            }
+        }
     }
 }
