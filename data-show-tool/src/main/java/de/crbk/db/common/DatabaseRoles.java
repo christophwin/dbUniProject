bf2aad97d59de1e761c77fdf8bc59f2eb7f12576
christophwin <christophwinter97@gmail.com>
2018-06-10 16:42:30 +0200

    added database roles

---
+++ data-show-tool/src/main/java/de/crbk/db/common/DatabaseRoles.java
@@ -0,0 +1,10 @@
+package de.crbk.db.common;
+
+public class DatabaseRoles
+{
+    public static final String ASSISTANT = "assistant";
+
+    public static final String PROFESSOR = "professor";
+
+    public static final String STUDENT = "student";
+}
